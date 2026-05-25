4b2f0840e891b80101e2ab2e2e8ed75afc796078
porpora02-ai <porpora02@aol.com>
2026-05-24 21:07:57 -0700

    Update user.js

--- user.js
+++ user.js
@@ -1,21 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-
-    email: {
-        type: String,
-        required: true
-    },
-
-    password: {
-        type: String,
-        required: true
-    }
+    username: { type: String, unique: true, required: true },
+    email: String,
+    password: String
 });
 
 module.exports = mongoose.model("User", UserSchema);
