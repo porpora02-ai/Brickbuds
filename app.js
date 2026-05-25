14f00943f7cb41478e3f4cc66e62fd9f6dddde99
porpora02-ai <porpora02@aol.com>
2026-05-24 21:08:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,40 @@
-async function register() {
+async function register(){
 
-    const username =
-    document.getElementById("username").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    const email =
-    document.getElementById("email").value;
+    const res = await fetch("/api/auth/register", {
+        method:"POST",
+        headers:{ "Content-Type":"application/json" },
+        body: JSON.stringify({ username, email, password })
+    });
 
-    const password =
-    document.getElementById("password").value;
+    const data = await res.json();
 
-    const response = await fetch(
-        "http://localhost:3000/api/auth/register",
-        {
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            })
-        }
-    );
-
-    const data = await response.json();
-
-    document.getElementById("message")
-    .innerText = data.message;
+    document.getElementById("message").innerText = data.message;
 
     if(data.success){
-
-        window.location.href = "games.html";
+        window.location.href = "/games";
     }
 }
 
-async function login() {
+async function login(){
 
-    const username =
-    document.getElementById("username").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    const password =
-    document.getElementById("password").value;
+    const res = await fetch("/api/auth/login", {
+        method:"POST",
+        headers:{ "Content-Type":"application/json" },
+        body: JSON.stringify({ username, password })
+    });
 
-    const response = await fetch(
-        "http://localhost:3000/api/auth/login",
-        {
-            method: "POST",
+    const data = await res.json();
 
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify({
-                username,
-                password
-            })
-        }
-    );
-
-    const data = await response.json();
-
-    document.getElementById("message")
-    .innerText = data.message;
+    document.getElementById("message").innerText = data.message;
 
     if(data.success){
-
-        window.location.href = "games.html";
+        window.location.href = "/games";
     }
 }
